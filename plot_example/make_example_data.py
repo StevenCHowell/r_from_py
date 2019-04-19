25afdc53db611dd543957ef6d5b1f371b5750715
Steven C. Howell <StevenCHowell@users.noreply.github.com>
2019-04-19 11:33:54 -0400

    move data to its own folder

--- plot_example/make_example_data.py
+++ plot_example/make_example_data.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 
 x = np.linspace(0, 2*np.pi, 100)
-pd.DataFrame(np.c_[x, np.sin(x)]).to_csv('sin.csv', index=False)
-pd.DataFrame(np.c_[x, np.cos(x)]).to_csv('cos.csv', index=False)
-pd.DataFrame(np.c_[x, np.sinc(x)]).to_csv('sinc.csv', index=False)
-pd.DataFrame(np.c_[x, np.sin(x)**2]).to_csv('sin2.csv', index=False)
-pd.DataFrame(np.c_[x, np.cos(x)**2]).to_csv('cos2.csv', index=False)
+pd.DataFrame(np.c_[x, np.sin(x)]).to_csv('data/sin.csv', index=False)
+pd.DataFrame(np.c_[x, np.cos(x)]).to_csv('data/cos.csv', index=False)
+pd.DataFrame(np.c_[x, np.sinc(x)]).to_csv('data/sinc.csv', index=False)
+pd.DataFrame(np.c_[x, np.sin(x)**2]).to_csv('data/sin2.csv', index=False)
+pd.DataFrame(np.c_[x, np.cos(x)**2]).to_csv('data/cos2.csv', index=False)
